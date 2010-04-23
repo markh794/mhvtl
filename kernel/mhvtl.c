023a6d629c4f5678437f490c2e1629374928e2bb
Mark Harvey <markh794@gmail.com>
2010-04-23 17:45:13 +1000

    kernel module: Bump vers string/date
    module version now 0.18.6
    module date is now 20100422-0
    
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- kernel/mhvtl.c
+++ kernel/mhvtl.c
@@ -100,9 +100,9 @@ struct scatterlist;
 #define WRITE_ATTRIBUTE 0x8d
 #define SECURITY_PROTOCOL_OUT 0xb5
 #ifndef MHVTL_VERSION
-#define MHVTL_VERSION "0.18.2"
+#define MHVTL_VERSION "0.18.6"
 #endif
-static const char *vtl_version_date = "20100326-0";
+static const char *vtl_version_date = "20100422-0";
 static const char vtl_driver_name[] = "mhvtl";
 
 /* Additional Sense Code (ASC) used */
