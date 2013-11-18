f820c745a79f86b5244c112049af808674e1ecc6
Mark Harvey <markh794@gmail.com>
2013-11-18 14:32:18 +1100

    SMC: Add OVERLAND library personality module
    Found manual:
    ftp://www.softdevlabs.com/fish/pub/SCSI/manuals/Overland/Overland%20L490E%20SCSI%20Manual.pdf
    
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- usr/vtllibrary.c
+++ usr/vtllibrary.c
@@ -1511,6 +1511,8 @@ static void customise_lu(struct lu_phy_attr *lu)
 		}
 	else if (!strncasecmp(lu->vendor_id, "HP", 2))
 		init_hp_eml_smc(lu);
+	else if (!strncasecmp(lu->product_id, "OVERLAND", 8))
+		init_overland_smc(lu);
 	else if (!strncasecmp(lu->product_id, "scalar", 6))
 		init_scalar_smc(lu);
 	else if (!strncasecmp(lu->vendor_id, "SPECTRA ", 7) &&
