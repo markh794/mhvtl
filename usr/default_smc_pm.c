a0047126efc937cb4f738ae9c40a547a37c40fc8
Mark Harvey <markh794@gmail.com>
2014-07-28 08:48:47 +1000

    Resolve an issue with default library emulation segfault
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- usr/default_smc_pm.c
+++ usr/default_smc_pm.c
@@ -1,6 +1,5 @@
 /*
- * Personality module for STK L series of robots
- * e.g. L180, L700, L20/40/80
+ * Personality module for default emulation
  */
 
 #include <stdio.h>
@@ -20,6 +19,23 @@ static struct smc_personality_template smc_pm = {
 	.dvcid_len			= 32,
 };
 
+static void update_default_inquiry(struct lu_phy_attr *lu)
+{
+	struct smc_priv *smc_p;
+	smc_p = lu->lu_private;
+
+	lu->inquiry[2] = 5;	/* SNSI Approved Version */
+	lu->inquiry[3] = 2;	/* Response data format */
+	lu->inquiry[4] = 0x43;	/* Additional length */
+
+	memcpy(&lu->inquiry[38], &lu->lu_serial_no, 12);
+	lu->inquiry[55] |= smc_p->pm->library_has_barcode_reader ? 1 : 0;
+	put_unaligned_be16(0x005c, &lu->inquiry[58]); /* SAM-2 */
+	put_unaligned_be16(0x0b56, &lu->inquiry[60]); /* SPI-4 */
+	put_unaligned_be16(0x02fe, &lu->inquiry[62]); /* SMC-2 */
+	put_unaligned_be16(0x030f, &lu->inquiry[64]); /* SPC-3 */
+}
+
 void init_default_smc(struct  lu_phy_attr *lu)
 {
 	smc_pm.name = "mhVTL - Default emulation";
@@ -29,7 +45,7 @@ void init_default_smc(struct  lu_phy_attr *lu)
 	smc_pm.dvcid_serial_only = FALSE;
 
 	smc_pm.start_drive	= 0x001;
-	smc_pm.start_picker	= 0x02c;
+	smc_pm.start_picker	= 0x2f0;
 	smc_pm.start_map	= 0x300;
 	smc_pm.start_storage	= 0x400;
 
@@ -37,6 +53,7 @@ void init_default_smc(struct  lu_phy_attr *lu)
 	smc_personality_module_register(&smc_pm);
 
 	init_slot_info(lu);
+	update_default_inquiry(lu);
 
 	init_smc_log_pages(lu);
 	init_smc_mode_pages(lu);
