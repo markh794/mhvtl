b14c7aeafa6347f43b759ca9f8c207b6ffdcf16f
Mark Harvey <markh794@gmail.com>
2015-03-02 14:56:24 +1100

    Replace tcopy with BSD version (tcopy-linux)
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- tcopy/tcopy.c
+++ tcopy/tcopy.c
@@ -1,203 +1,346 @@
+/*
+ * Copyright (c) 1985, 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+/* Linux port - Nicholas Harbour 2000 */
 
+#ifndef lint
+static const char copyright[] =
+"@(#) Copyright (c) 1985, 1987, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+#if 0
+static char sccsid[] = "@(#)tcopy.c	8.2 (Berkeley) 4/17/94";
+#endif
+static const char rcsid[] =
+  "$FreeBSD: src/usr.bin/tcopy/tcopy.c,v 1.2.2.4 1999/09/05 11:33:13 peter Exp $";
+#endif /* not lint */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/mtio.h>
+
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/param.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/io.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <linux/mtio.h>
 
-/* Buffer size larger than any block we are expecting to read in one chunk */
-#define BUF_SZ (1024 * 600)	/* 600K seems like a reasonable value */
+#include "pathnames.h"
 
-char cBuf[BUF_SZ];
+#define	MAXREC	(64 * 1024)
+#define	NOCOUNT	(-2)
 
-/*
- * Take error num and provide a more user friendly text error msg
- */
-int iErr(int id)
+int	filen, guesslen, maxblk = MAXREC;
+u_long	lastrec, record, size, tsize;
+/*FILE	*msg = stdout;*/
+FILE *msg;  /*njh 2000*/
+
+void	*getspace __P((int));
+void	 intr __P((int));
+static void	 usage __P((void));
+void	 verify __P((int, int, char *));
+void	 writeop __P((int, int));
+
+int
+main(argc, argv)
+	int argc;
+	char *argv[];
 {
-	switch (id) {
-	case EIO:
-		fprintf(stderr, " The requested operation could not be completed.\n");
-		goto fail;
-		break;
-	case ENOSPC:
-		fprintf(stderr, " A write operation could not be completed because the tape reached end-of-medium.\n");
-		goto fail;
-		break;
-	case EACCES:
-		fprintf(stderr, " An attempt was made to write or erase a write-protected tape.\n");
-		goto fail;
-		break;
-	case EFAULT:
-		fprintf(stderr, " The command parameters point to memory not belonging to the calling process.\n");
-		goto fail;
-		break;
-	case ENXIO:
-		fprintf(stderr, "During opening, the tape device does not exist.\n");
-		goto fail;
-		break;
-	case EBUSY:
-		fprintf(stderr, "The device is already in use or the driver was unable to allocate a buffer.\n");
-		goto fail;
-		break;
-	case EOVERFLOW:
-		fprintf(stderr, "An attempt was made to read or write a variable-length block that is larger than the driver's internal buffer.\n");
-		goto fail;
-		break;
-	case EINVAL:
-		fprintf(stderr, "An ioctl had an illegal argument, or a requested block size was illegal.\n");
-		goto fail;
-		break;
-	case ENOSYS:
-		fprintf(stderr, "Unknown ioctl\n");
-		break;
-	case EROFS:
-		fprintf(stderr, "Open is attempted with O_WRONLY or O_RDWR when the tape in the drive is write-protected.\n");
-		goto fail;
-		break;
-	}
-	return id;			/* set the return status */
-fail:
-	exit(-id);
-}
-int main(int argc, char **argv)
-{
-	int res, length;
-	ssize_t idIn, idOut;
-	int iFirst = 0, iLast = 0;
-	int iOldByte = 0;
-	int eof = 0, iFile = 1, iBlock = 0, iF = 0, iError = 0;
-	struct mtop mt_cmd;
-
-	mt_cmd.mt_op = MTWEOF;
-	mt_cmd.mt_count = 1;
-
-	fprintf(stderr, "*** Written by Odin Explorer programmer\n");
-
-	if (argc == 1) {
-		fprintf(stderr, "!!! Usage : %s input output\n", argv[0]);
-		exit(1);
-	}
-	if (argc == 3) {
-		idOut = open(argv[2], O_WRONLY);
-		if (idOut == -1) {
-			fprintf(stderr, "Can't open output file %s\n", argv[2]);
-			exit(2);
-		}
-		/* Set variable block size on target tape drive */
-		mt_cmd.mt_op    = MTSETBLK;
-		mt_cmd.mt_count = 0;
-		iErr(ioctl(idOut, MTIOCTOP, &mt_cmd));
-
-		/* Set drive buffering per SCSI-2 on target tape drive */
-		mt_cmd.mt_op    = MTSETDRVBUFFER;
-		mt_cmd.mt_count = MT_ST_BOOLEANS | !MT_ST_ASYNC_WRITES;
-		iErr(ioctl(idOut, MTIOCTOP, &mt_cmd));
-	}
-	idIn = open(argv[1], O_RDONLY);
-	if (idIn == -1) {
-		fprintf(stderr, "Can't open input file %s\n", argv[1]);
-		exit(2);
-	}
-
-	/* Set variable block size of source tape drive */
-	mt_cmd.mt_op    = MTSETBLK;
-	mt_cmd.mt_count = 0;
-	iErr(ioctl(idIn, MTIOCTOP, &mt_cmd));
-
-	while (1) {
-		if ((res = read(idIn, &cBuf, BUF_SZ)) == -1) {
-			printf("Read Failed. %s (%d), Ret val: %d, Error count %d\n",
-					strerror(errno), errno, res, iError);
-			iError++;
-			if (iError < 3) {
-				sleep(1);
-				continue;
+	register int lastnread, nread, nw, inp, outp;
+	enum {READ, VERIFY, COPY, COPYVERIFY} op = READ;
+	sig_t oldsig;
+	int ch, needeof;
+	char *buff, *inf;
+	
+	msg = stdout;
+	guesslen = 1;
+	while ((ch = getopt(argc, argv, "cs:vx")) != -1)
+		switch((char)ch) {
+		case 'c':
+			op = COPYVERIFY;
+			break;
+		case 's':
+			maxblk = atoi(optarg);
+			if (maxblk <= 0) {
+				warnx("illegal block size");
+				usage();
 			}
-			exit(1);
+			guesslen = 0;
+			break;
+		case 'v':
+			op = VERIFY;
+			break;
+		case 'x':
+			msg = stderr;
+			break;
+		case '?':
+		default:
+			usage();
 		}
-		iError = 0;
-		if (!res) {
-			if (eof) {
-				break;
-			} else {
-				eof = 1;
-				if (iFirst == iLast)
-					fprintf(stderr, " tcopy: Tape File %d; Record %d; Size %d\n", iFile, iBlock, iOldByte);
+	argc -= optind;
+	argv += optind;
+
+	switch(argc) {
+	case 0:
+		if (op != READ)
+			usage();
+		inf = _PATH_DEFTAPE;
+		break;
+	case 1:
+		if (op != READ)
+			usage();
+		inf = argv[0];
+		break;
+	case 2:
+		if (op == READ)
+			op = COPY;
+		inf = argv[0];
+		if ((outp = open(argv[1], op == VERIFY ? O_RDONLY :
+		    op == COPY ? O_WRONLY : O_RDWR, DEFFILEMODE)) < 0)
+			err(3, "%s", argv[1]);
+		break;
+	default:
+		usage();
+	}
+
+	if ((inp = open(inf, O_RDONLY, 0)) < 0)
+		err(1, "%s", inf);
+
+	buff = getspace(maxblk);
+
+	if (op == VERIFY) {
+		verify(inp, outp, buff);
+		exit(0);
+	}
+
+	if ((oldsig = signal(SIGINT, SIG_IGN)) != SIG_IGN)
+		(void) signal(SIGINT, intr);
+
+	needeof = 0;
+	for (lastnread = NOCOUNT;;) {
+		if ((nread = read(inp, buff, maxblk)) == -1) {
+			while (errno == EINVAL && (maxblk -= 1024)) {
+				nread = read(inp, buff, maxblk);
+				if (nread >= 0)
+					goto r1;
+			}
+			err(1, "read error, file %d, record %ld", filen, record);
+		} else if (nread != lastnread) {
+			if (lastnread != 0 && lastnread != NOCOUNT) {
+				if (lastrec == 0 && nread == 0)
+					fprintf(msg, "%ld records\n", record);
+				else if (record - lastrec > 1)
+					fprintf(msg, "records %ld to %ld\n",
+					    lastrec, record);
 				else
-					fprintf(stderr, " tcopy: Tape File %d; Records: %d to %d; Size: %d\n", iFile, iFirst, iLast, iOldByte);
-				fprintf(stderr, "************************************************************\n");
-				iFile++;
-				iBlock = 0;
-				if (argc == 3) {
-					/* Write 1 filemark to destination tape */
-					mt_cmd.mt_op    = MTWEOF;
-					mt_cmd.mt_count = 1;
-					iErr(ioctl(idOut, MTIOCTOP, &mt_cmd));
+					fprintf(msg, "record %ld\n", lastrec);
+			}
+			if (nread != 0)
+				fprintf(msg, "file %d: block size %d: ",
+				    filen, nread);
+			(void) fflush(stdout);
+			lastrec = record;
+		}
+r1:		guesslen = 0;
+		if (nread > 0) {
+			if (op == COPY || op == COPYVERIFY) {
+				if (needeof) {
+					writeop(outp, MTWEOF);
+					needeof = 0;
 				}
-				continue;
+				nw = write(outp, buff, nread);
+				if (nw != nread) {
+					if (nw == -1) {
+					warn("write error, file %d, record %ld", filen, record);
+					} else {
+					warnx("write error, file %d, record %ld", filen, record);
+					warnx("write (%d) != read (%d)", nw, nread);
+					}
+					errx(5, "copy aborted");
+				}
 			}
-		}
-		eof = 0;
-		iBlock++;
-		length = res;
-		if (argc == 3) {
-			res = write(idOut, &cBuf, length);
-			if (res != length) {
-				printf("Error writing to the file. res = %d length = %d\n", res, length);
-				exit(1);
+			size += nread;
+			record++;
+		} else {
+			if (lastnread <= 0 && lastnread != NOCOUNT) {
+				fprintf(msg, "eot\n");
+				break;
 			}
+			fprintf(msg,
+			    "file %d: eof after %lu records: %lu bytes\n",
+			    filen, record, size);
+			needeof = 1;
+			filen++;
+			tsize += size;
+			size = record = lastrec = 0;
+			lastnread = 0;
 		}
-
-		if (iBlock == 1) {
-			iOldByte = res;
-			iFirst = 1;
-			iLast = 1;
-			continue;
-		}
-		if (res == iOldByte) {
-			if (!iF) {
-				iF = 1;
-				iFirst = iBlock-1;
-			}
-			iLast = iBlock;
-			continue;
-		}
-		if (iFirst == iLast)
-			fprintf(stderr, " tcopy: Tape File %d; Record: %d; Size %d\n", iFile, iBlock-1, iOldByte);
-		else {
-			fprintf(stderr, " tcopy: Tape File %d; Records: %d to %d; Size: %d\n", iFile, iFirst, iLast, iOldByte);
-			iFirst = iLast;
-		}
-		iOldByte = res;
-		iF = 0;
+		lastnread = nread;
 	}
-	fprintf(stderr, " tcopy: The end of tape is reached. Files on tape: %d\n", iFile-1);
-
-	/* Eject source tape */
-	mt_cmd.mt_op    = MTOFFL;
-	mt_cmd.mt_count = 1;
-	iErr(ioctl(idIn, MTIOCTOP, &mt_cmd));
-	close(idIn);
-	if (argc == 3) {
-		/* Write 1 filemark to target */
-		mt_cmd.mt_op    = MTWEOF;
-		mt_cmd.mt_count = 1;
-		iErr(ioctl(idOut, MTIOCTOP, &mt_cmd));
-
-		/* eject target tape */
-		mt_cmd.mt_op    = MTOFFL;
-		mt_cmd.mt_count = 1;
-		iErr(ioctl(idOut, MTIOCTOP, &mt_cmd));
-
-		close(idOut);
+	fprintf(msg, "total length: %lu bytes\n", tsize);
+	(void)signal(SIGINT, oldsig);
+	if (op == COPY || op == COPYVERIFY) {
+		writeop(outp, MTWEOF);
+		writeop(outp, MTWEOF);
+		if (op == COPYVERIFY) {
+			writeop(outp, MTREW);
+			writeop(inp, MTREW);
+			verify(inp, outp, buff);
+		}
 	}
 	exit(0);
 }
+
+void
+verify(inp, outp, outb)
+	register int inp, outp;
+	register char *outb;
+{
+	register int eot, inmaxblk, inn, outmaxblk, outn;
+	register char *inb;
+
+	inb = getspace(maxblk);
+	inmaxblk = outmaxblk = maxblk;
+	for (eot = 0;; guesslen = 0) {
+		if ((inn = read(inp, inb, inmaxblk)) == -1) {
+			if (guesslen)
+				while (errno == EINVAL && (inmaxblk -= 1024)) {
+					inn = read(inp, inb, inmaxblk);
+					if (inn >= 0)
+						goto r1;
+				}
+			warn("read error");
+			break;
+		}
+r1:		if ((outn = read(outp, outb, outmaxblk)) == -1) {
+			if (guesslen)
+				while (errno == EINVAL && (outmaxblk -= 1024)) {
+					outn = read(outp, outb, outmaxblk);
+					if (outn >= 0)
+						goto r2;
+				}
+			warn("read error");
+			break;
+		}
+r2:		if (inn != outn) {
+			fprintf(msg,
+			    "%s: tapes have different block sizes; %d != %d.\n",
+			    "tcopy", inn, outn);
+			break;
+		}
+		if (!inn) {
+			if (eot++) {
+				fprintf(msg, "tcopy: tapes are identical.\n");
+				return;
+			}
+		} else {
+			if (bcmp(inb, outb, inn)) {
+				fprintf(msg,
+				    "tcopy: tapes have different data.\n");
+				break;
+			}
+			eot = 0;
+		}
+	}
+	exit(1);
+}
+
+void
+intr(signo)
+	int signo;
+{
+	if (record)
+		if (record - lastrec > 1)
+			fprintf(msg, "records %ld to %ld\n", lastrec, record);
+		else
+			fprintf(msg, "record %ld\n", lastrec);
+	fprintf(msg, "interrupt at file %d: record %ld\n", filen, record);
+	fprintf(msg, "total length: %ld bytes\n", tsize + size);
+	exit(1);
+}
+
+void *
+getspace(blk)
+	int blk;
+{
+	void *bp;
+
+	if ((bp = malloc((size_t)blk)) == NULL)
+		errx(11, "no memory");
+	return (bp);
+}
+
+void
+writeop(fd, type)
+	int fd, type;
+{
+	struct mtop op;
+
+	op.mt_op = type;
+	op.mt_count = (daddr_t)1;
+	if (ioctl(fd, MTIOCTOP, (char *)&op) < 0)
+		err(6, "tape op");
+}
+
+static void
+usage()
+{
+	fprintf(stderr, "usage: tcopy [-cvx] [-s maxblk] [src [dest]]\n");
+	exit(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
