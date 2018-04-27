e574eca67f9e11c1102fa78abb0b0a056fa5a91b
Julien Leduc <julien.leduc@cern.ch>
2018-04-27 11:23:38 +0200

    Fixing <4.15 kernel module compilation

--- kernel/mhvtl.c
+++ kernel/mhvtl.c
@@ -742,7 +742,8 @@ static void timer_intr_handler(struct timer_list *t)
 	unsigned long indx = sqcp->a_cmnd->serial_number;
 #else
 static void timer_intr_handler(unsigned long indx)
-  struct vtl_queued_cmd *sqcp = NULL;
+{
+	struct vtl_queued_cmd *sqcp = NULL;
 #endif
 	struct vtl_lu_info *lu;
 
