a636fdf50e610f7534a830efe690259b8441fe1e
Mark Harvey <markh794@gmail.com>
2018-04-29 11:25:49 +1000

    Bump kernel module version strings
    Add a method to identify if source will compile with 4.14+ kernels
    
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- kernel/mhvtl.c
+++ kernel/mhvtl.c
@@ -100,9 +100,9 @@ struct scatterlist;
  #define VTL_VERSION "1.75"
 */
 #ifndef MHVTL_VERSION
-#define MHVTL_VERSION "0.18.17"
+#define MHVTL_VERSION "0.18.18"
 #endif
-static const char *vtl_version_date = "20150414-0";
+static const char *vtl_version_date = "20180429-0";
 static const char vtl_driver_name[] = "mhvtl";
 
 /* Additional Sense Code (ASC) used */
