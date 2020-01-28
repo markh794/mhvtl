08dc27acde27e0a9547cc41fa24fc3c34f5b5390
Mark Harvey <markh794@gmail.com>
2020-01-28 16:06:49 +1100

    mhvtl.ko: bump kernel module version
    Make it easier to identify if the bounce-buffer size is increased.
    
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- kernel/mhvtl.c
+++ kernel/mhvtl.c
@@ -101,9 +101,9 @@ struct scatterlist;
  #define VTL_VERSION "1.75"
 */
 #ifndef MHVTL_VERSION
-#define MHVTL_VERSION "0.18.23"
+#define MHVTL_VERSION "0.18.24"
 #endif
-static const char *vtl_version_date = "20191004-0";
+static const char *vtl_version_date = "20200128-0";
 static const char vtl_driver_name[] = "mhvtl";
 
 /* Additional Sense Code (ASC) used */
