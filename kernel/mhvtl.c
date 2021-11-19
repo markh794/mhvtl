9131f216f8ba13576313c37e229c831aa5c6efce
Mark Harvey <markh794@gmail.com>
2021-11-19 16:18:24 +1100

    mhvtl.ko: Bump kernel version to 0.18.31 / 2021-11-19
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- kernel/mhvtl.c
+++ kernel/mhvtl.c
@@ -101,9 +101,9 @@ struct scatterlist;
  #define VTL_VERSION "1.75"
 */
 #ifndef MHVTL_VERSION
-#define MHVTL_VERSION "0.18.30"
+#define MHVTL_VERSION "0.18.31"
 #endif
-static const char *mhvtl_version_date = "20211007-0";
+static const char *mhvtl_version_date = "20211119-0";
 static const char mhvtl_driver_name[] = "mhvtl";
 
 /* Additional Sense Code (ASC) used */
