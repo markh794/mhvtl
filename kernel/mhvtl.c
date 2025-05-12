94d8d07c8d7177b800f14fd68d10b196d02c0a3e
Mark Harvey <markh794@gmail.com>
2025-05-12 12:53:55 +1000

    mhvtl.ko: Bump version string to account for SPIN_LOCK_ fixes
    Signed-off-by: Mark Harvey <markh794@gmail.com>

--- kernel/mhvtl.c
+++ kernel/mhvtl.c
@@ -104,9 +104,9 @@ struct scatterlist;
  #define VTL_VERSION "1.75"
 */
 #ifndef MHVTL_VERSION
-#define MHVTL_VERSION "0.18.34"
+#define MHVTL_VERSION "0.18.35"
 #endif
-static const char *mhvtl_version_date = "20250212-0";
+static const char *mhvtl_version_date = "20250512-0";
 static const char mhvtl_driver_name[] = "mhvtl";
 
 /* Additional Sense Code (ASC) used */
